963c37a415fbaf231b4693fd171caf05b8d987c4
savitaashture <sashture@redhat.com>
2023-12-06 16:12:34 +0530

    Update main.go

--- main.go
+++ main.go
@@ -9,16 +9,21 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	// Extract the path variable from the URL
+	fmt.Println("first")
 	path := strings.TrimPrefix(r.URL.Path, "/")
 
+	fmt.Println("second", path)
 	// Generate an HTML response with tags
 	htmlResponse := fmt.Sprintf("<html><body><h1>Hello, It's Kubeday India 2023!!! and we are deploying image version %s</h1></body></html>", path)
 
+	fmt.Println("third", htmlResponse)
 	// Write the HTML response
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(htmlResponse))
 	_, err := w.Write([]byte(htmlResponse))
-	log.Fatal(err)
+	fmt.Println(err)
+	fmt.Println("at the end")
 }
 
 func main() {
